aa5fe1c4cdd6bd54a4799733defa9f4b4b517129
Cho-D-YoungRae <49931252+Cho-D-YoungRae@users.noreply.github.com>
2024-04-02 01:48:21 +0900

    Feature/#4 flyway seperation (#5)
    * chore: api application 모듈 하위로 이동
    
    * chore: domain-rds application 모듈 하위로 이동
    
    * chore: mysql 의존성 추가
    
    * chore: security 의존성 제거
    
    * chore: flyway, mysql 의존성 제거
    
    * chore: flyway 분리
    
    * chore: flyway 프로퍼티 제거

---
+++ application/rds-migration/src/main/java/com/nocommittoday/techswipe/migration/rds/TechSwipeRdsMigrationApplication.java
@@ -0,0 +1,12 @@
+package com.nocommittoday.techswipe.migration.rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSwipeRdsMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSwipeRdsMigrationApplication.class, args);
+    }
+}
