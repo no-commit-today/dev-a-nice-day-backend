33db8613f8b0101a6890a41fcbc3faa2fdf7a567
Youngrae Cho <yrc9229@gmail.com>
2024-05-12 00:25:20 +0900

    style: TechPost 롬복 적용

--- application/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/content/TechPost.java
+++ application/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/content/TechPost.java
@@ -1,6 +1,7 @@
 package com.nocommittoday.techswipe.domain.rds.content;
 
 import com.nocommittoday.techswipe.domain.rds.core.BaseSoftDeleteEntity;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,11 +10,17 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
+
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Table(
@@ -26,6 +33,8 @@ import java.util.UUID;
                 @Index(name = "ix_tech_post__published_at", columnList = "published_at")
         }
 )
+@Getter
+@NoArgsConstructor(access = PROTECTED)
 public class TechPost extends BaseSoftDeleteEntity {
 
     @Column(name = "uid", length = 45, nullable = false)
@@ -49,121 +58,45 @@ public class TechPost extends BaseSoftDeleteEntity {
     private LocalDateTime publishedAt;
 
     @OneToMany(mappedBy = "post", orphanRemoval = true, cascade = CascadeType.ALL)
-    private List<TechPostKeyword> keywords;
+    private List<TechPostKeyword> keywords = new ArrayList<>();
 
     @OneToMany(mappedBy = "post", orphanRemoval = true, cascade = CascadeType.ALL)
-    private List<TechPostCategory> categories;
-
-    public static TechPostBuilder builder() {
-        return new TechPostBuilder();
-    }
-
-    protected TechPost() {
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public LocalDateTime getPublishedAt() {
-        return publishedAt;
-    }
+    private List<TechPostCategory> categories = new ArrayList<>();
 
     public List<TechPostKeyword> getKeywords() {
-        return keywords;
+        return Collections.unmodifiableList(keywords);
     }
 
     public List<TechPostCategory> getCategories() {
-        return categories;
+        return Collections.unmodifiableList(categories);
     }
 
-    public static final class TechPostBuilder {
-
-        private String link;
-        private String title;
-        private String content;
-        private String summary;
-        private LocalDateTime publishedAt;
-        private List<TechKeyword> keywords = List.of();
-        private List<TechCategory> categories = List.of();
-
-        public TechPostBuilder() {
-        }
-
-        public TechPostBuilder link(final String link) {
-            this.link = link;
-            return this;
-        }
-
-        public TechPostBuilder title(final String title) {
-            this.title = title;
-            return this;
-        }
-
-        public TechPostBuilder content(final String content) {
-            this.content = content;
-            return this;
-        }
-
-        public TechPostBuilder summary(final String summary) {
-            this.summary = summary;
-            return this;
-        }
-
-        public TechPostBuilder publishedAt(final LocalDateTime publishedAt) {
-            this.publishedAt = publishedAt;
-            return this;
-        }
-
-        public TechPostBuilder keywords(final List<TechKeyword> keywords) {
-            if (keywords == null) {
-                this.keywords = List.of();
-                return this;
-            }
-            this.keywords = new ArrayList<>(keywords);
-            return this;
-        }
-
-        public TechPostBuilder categories(final List<TechCategory> categories) {
-            if (categories == null) {
-                this.categories = List.of();
-                return this;
-            }
-            this.categories = new ArrayList<>(categories);
-            return this;
-        }
-
-        public TechPost build() {
-            final TechPost techPost = new TechPost();
-            techPost.uid = UUID.randomUUID().toString();
-            techPost.link = this.link;
-            techPost.title = this.title;
-            techPost.content = this.content;
-            techPost.summary = this.summary;
-            techPost.publishedAt = this.publishedAt;
-            techPost.keywords = this.keywords.stream()
-                    .map(keyword -> new TechPostKeyword(techPost, keyword))
-                    .toList();
-            techPost.categories = this.categories.stream()
-                    .map(category -> new TechPostCategory(techPost, category))
-                    .toList();
-            return techPost;
-        }
+    @Builder
+    public TechPost(
+            final String uid,
+            final String link,
+            final String title,
+            final String content,
+            final String summary,
+            final LocalDateTime publishedAt,
+            @Nullable final List<TechKeyword> keywords,
+            @Nullable final List<TechCategory> categories
+    ) {
+        this.uid = uid;
+        this.link = link;
+        this.title = title;
+        this.content = content;
+        this.summary = summary;
+        this.publishedAt = publishedAt;
+        this.keywords = Optional.ofNullable(keywords)
+                .map(item -> item.stream()
+                        .map(keyword -> new TechPostKeyword(this, keyword))
+                        .toList()
+                ).orElse(List.of());
+        this.categories = Optional.ofNullable(categories)
+                .map(item -> item.stream()
+                        .map(category -> new TechPostCategory(this, category))
+                        .toList()
+                ).orElse(List.of());
     }
 }
