568ade899d5b8796c693d1069d731e2424d90e14
Youngrae Cho <yrc9229@gmail.com>
2024-05-19 05:09:19 +0900

    feat: html index 로 url 리스트 크롤링 추가

---
+++ application/batch/src/test/java/com/nocommittoday/techswipe/batch/service/TechPostUrlListHtmlIndexCrawlingIteratorRealTest.java
@@ -0,0 +1,25 @@
+package com.nocommittoday.techswipe.batch.service;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Tag("real")
+class TechPostUrlListHtmlIndexCrawlingIteratorRealTest {
+
+    @Test
+    void 리디() {
+        final TechPostUrlListHtmlIndexCrawlingIterator iterator = new TechPostUrlListHtmlIndexCrawlingIterator(
+                List.of(11, 4, 1, 0),
+                "https://ridicorp.com/story-category/tech-blog/",
+                "https://ridicorp.com/story-category/tech-blog/page/%d/"
+        );
+        final List<String> urlList = new ArrayList<>();
+        while (iterator.hasNext()) {
+            urlList.add(iterator.next());
+        }
+        System.out.println(urlList);
+    }
+}
