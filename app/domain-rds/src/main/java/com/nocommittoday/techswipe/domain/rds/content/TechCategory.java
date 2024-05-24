066aae48aeebba991aab85b026e3779f81810082
Youngrae Cho <yrc9229@gmail.com>
2024-05-24 15:36:46 +0900

    prune: module 하위 domain code 관련 모듈 제거

--- app/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/content/TechCategory.java
+++ app/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/content/TechCategory.java
@@ -1,33 +1,13 @@
 package com.nocommittoday.techswipe.domain.rds.content;
 
-import com.nocommittoday.module.domain.code.DomainCode;
 
-/**
- * 기타 카테고리는 추후 데이터 확인 후 다른 카테고리로 분리될 수 있음
- */
-public enum TechCategory implements DomainCode {
+public enum TechCategory {
 
-    BACKEND("백엔드"),
-    FRONTEND("프론트엔드"),
-    ANDROID("안드로이드"),
-    IOS("iOS"),
-    INFRA("인프라"),
-    ETC("기타"),
-    ;
+    BACKEND,
+    FRONTEND,
+    ANDROID,
+    IOS,
+    INFRA,
+    ETC,
 
-    private final String title;
-
-    TechCategory(final String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String getCode() {
-        return name();
-    }
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
 }
--- app/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/provider/TechBlogType.java
+++ app/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/provider/TechBlogType.java
@@ -1,26 +1,8 @@
 package com.nocommittoday.techswipe.domain.rds.provider;
 
-import com.nocommittoday.module.domain.code.DomainCode;
-
-public enum TechBlogType implements DomainCode {
+public enum TechBlogType {
 
     // 국내 기업
-    DOMESTIC_COMPANY("국내 기업"),
-    ;
+    DOMESTIC_COMPANY,
 
-    private final String title;
-
-    TechBlogType(final String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String getCode() {
-        return name();
-    }
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
 }
