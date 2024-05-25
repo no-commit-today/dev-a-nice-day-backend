205b8403204f6c855fab9f4fbd9125c0abf14fd5
Youngrae Cho <yrc9229@gmail.com>
2024-05-25 19:48:40 +0900

    fix(content): provider 도메인 엔티티 누락된 필드 추가

--- app/content/content-domain/src/main/java/com/nocommittoday/techswipe/content/domain/TechContentProvider.java
+++ app/content/content-domain/src/main/java/com/nocommittoday/techswipe/content/domain/TechContentProvider.java
@@ -1,6 +1,30 @@
 package com.nocommittoday.techswipe.content.domain;
 
+import com.nocommittoday.techswipe.image.domain.Image;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.annotation.Nullable;
+
+@Getter
+@RequiredArgsConstructor
 public class TechContentProvider {
+
+    @NonNull
+    private final TechContentProviderId id;
+
+    @NonNull
+    private final TechContentProviderType type;
+
+    @NonNull
+    private final String title;
+
+    @NonNull
+    private final String url;
+
+    @Nullable
+    private final Image.ImageId iconId;
 
     public record TechContentProviderId(long value) { }
 }
