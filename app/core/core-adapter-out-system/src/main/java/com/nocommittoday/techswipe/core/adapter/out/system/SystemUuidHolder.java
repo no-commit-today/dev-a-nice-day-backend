a199540749ad6ba30381403c3ca7648a54bbc104
Youngrae Cho <yrc9229@gmail.com>
2024-05-25 23:58:38 +0900

    fix(chore): application 계층 누락된 의존성 추가

--- app/core/core-adapter-out-system/src/main/java/com/nocommittoday/techswipe/core/adapter/out/system/SystemUuidHolder.java
+++ app/core/core-adapter-out-system/src/main/java/com/nocommittoday/techswipe/core/adapter/out/system/SystemUuidHolder.java
@@ -1,9 +1,11 @@
 package com.nocommittoday.techswipe.core.adapter.out.system;
 
 import com.nocommittoday.techswipe.core.application.port.out.UuidHolder;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class SystemUuidHolder implements UuidHolder {
 
     @Override
