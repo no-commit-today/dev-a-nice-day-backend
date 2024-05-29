aa35d032374bc54b19a7cb92797c4329dff0adf6
Youngrae Cho <yrc9229@gmail.com>
2024-05-29 17:50:49 +0900

    feat(subscription): SubscriptionReaderAdapter 구현

---
+++ app/subscription/subscription-adapter-out-mysql/src/main/java/com/nocommittoday/techswipe/subscription/adapter/out/mysql/package-info.java
@@ -0,0 +1,6 @@
+@NonNullFields
+@NonNullApi
+package com.nocommittoday.techswipe.subscription.adapter.out.mysql;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
--- app/subscription/subscription-infra-mysql/src/main/java/com/nocommittoday/techswipe/subscription/infrastructure/mysql/SubscriptionJpaRepository.java
+++ app/subscription/subscription-infra-mysql/src/main/java/com/nocommittoday/techswipe/subscription/infrastructure/mysql/SubscriptionJpaRepository.java
@@ -2,5 +2,9 @@ package com.nocommittoday.techswipe.subscription.infrastructure.mysql;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SubscriptionJpaRepository extends JpaRepository<SubscriptionEntity, Long> {
+
+    Optional<SubscriptionEntity> findByProviderId(final TechContentProviderIdEmbeddable providerId);
 }
