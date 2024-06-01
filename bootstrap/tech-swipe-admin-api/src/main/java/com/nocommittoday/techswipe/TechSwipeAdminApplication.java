fa078602e0582c3b773449ac613984cddbf94af5
Youngrae Cho <yrc9229@gmail.com>
2024-06-02 01:59:10 +0900

    fix(bootstrap): tech-swipe-admin-api 추가

---
+++ bootstrap/tech-swipe-admin-api/src/main/java/com/nocommittoday/techswipe/TechSwipeAdminApplication.java
@@ -0,0 +1,14 @@
+package com.nocommittoday.techswipe;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSwipeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSwipeAdminApplication.class, args);
+    }
+
+}
