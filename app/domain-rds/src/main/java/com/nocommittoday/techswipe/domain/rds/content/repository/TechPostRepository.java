cee0d6928da4a244d11e369067676bee8b71a074
Youngrae Cho <yrc9229@gmail.com>
2024-06-06 02:22:09 +0900

    prune: 이전 app:domain-rds 제거

--- app/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/content/repository/TechPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nocommittoday.techswipe.domain.rds.content.repository;
-
-import com.nocommittoday.techswipe.domain.rds.content.TechPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechPostRepository extends JpaRepository<TechPost, Long>, TechPostRepositoryCustom {
-}
--- app/domain-rds/src/main/java/com/nocommittoday/techswipe/domain/rds/subscription/TechBlogSubscriptionType.java
+++
@@ -1,7 +0,0 @@
-package com.nocommittoday.techswipe.domain.rds.subscription;
-
-public enum TechBlogSubscriptionType {
-    RSS,
-    ATOM,
-    LIST_CRAWLING
-}
