dc5d39f6b65fa16d3a7ae55c21185c062cd5334f
Youngrae Cho <yrc9229@gmail.com>
2024-06-06 02:21:25 +0900

    prune: 이전 app:batch 제거

--- app/batch/src/main/java/com/nocommittoday/techswipe/batch/model/SubscribedTechPostList.java
+++
@@ -1,9 +0,0 @@
-package com.nocommittoday.techswipe.batch.model;
-
-import java.util.List;
-
-public record SubscribedTechPostList(
-        long techBlogId,
-        List<SubscribedTechPost> list
-) {
-}
--- app/batch/src/main/java/com/nocommittoday/techswipe/batch/service/FileStore.java
+++
@@ -1,8 +0,0 @@
-package com.nocommittoday.techswipe.batch.service;
-
-import org.springframework.core.io.Resource;
-
-public interface FileStore {
-
-    String store(Resource resource, String storedName);
-}
