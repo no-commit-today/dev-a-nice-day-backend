b791e926773c1af8643c3d5d938611f27c21667c
Youngrae Cho <yrc9229@gmail.com>
2024-06-06 18:07:37 +0900

    prune: package-info.java 제거
    - 매번 추가하기 현재는 번거로움
    - 추후 필요에 의해 추가해볼 수 있겠음

--- domain/content/content-application/src/main/java/com/nocommittoday/techswipe/content/application/port/in/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package com.nocommittoday.techswipe.content.application.port.in;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
