13032e86ca89888f9e1f78d3e3b59b76b92d4848
Youngrae Cho <yrc9229@gmail.com>
2024-06-09 04:44:06 +0900

    prune(collection): 카테고리 분류 유스케이스 제거
    - 배치에서만 실행되면 되므로 유스케이스는 제거
    - 카테고리 분류는 배치에서 진행

--- domain/collection/collection-application/src/main/java/com/nocommittoday/techswipe/collection/application/port/in/CollectionCategorizeUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.nocommittoday.techswipe.collection.application.port.in;
-
-import com.nocommittoday.techswipe.collection.domain.CollectedContent;
-
-public interface CollectionCategorizeUseCase {
-
-    void categorize(CollectedContent.CollectedContentId id);
-}
