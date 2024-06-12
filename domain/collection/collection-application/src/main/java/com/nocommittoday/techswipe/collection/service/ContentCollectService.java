540fb3cc73c2bc8770be67e98cfa30d66538f534
Youngrae Cho <yrc9229@gmail.com>
2024-06-13 00:53:59 +0900

    refactor(collection): out 포트 제거

--- domain/collection/collection-application/src/main/java/com/nocommittoday/techswipe/collection/service/ContentCollectService.java
+++ domain/collection/collection-application/src/main/java/com/nocommittoday/techswipe/collection/service/ContentCollectService.java
@@ -1,6 +1,5 @@
-package com.nocommittoday.techswipe.collection.application.service;
+package com.nocommittoday.techswipe.collection.service;
 
-import com.nocommittoday.techswipe.collection.application.port.in.ContentCollectUseCase;
 import com.nocommittoday.techswipe.collection.domain.vo.ContentCollect;
 import com.nocommittoday.techswipe.collection.infrastructure.CollectedContentAppender;
 import lombok.NonNull;
@@ -9,11 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-class ContentCollectService implements ContentCollectUseCase {
+class ContentCollectService {
 
     private final CollectedContentAppender collectedContentAppender;
 
-    @Override
     public void collect(final @NonNull ContentCollect command) {
         collectedContentAppender.save(command.toDomain());
     }
