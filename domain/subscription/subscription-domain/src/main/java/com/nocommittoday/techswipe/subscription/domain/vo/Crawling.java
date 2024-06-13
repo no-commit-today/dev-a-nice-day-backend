219473c1e8a244f18654a9a0f8035c4914717a3b
Youngrae Cho <yrc9229@gmail.com>
2024-06-14 00:22:02 +0900

    prune(subscription): Crawling 테스트 제거

--- domain/subscription/subscription-domain/src/main/java/com/nocommittoday/techswipe/subscription/domain/vo/Crawling.java
+++ domain/subscription/subscription-domain/src/main/java/com/nocommittoday/techswipe/subscription/domain/vo/Crawling.java
@@ -10,25 +10,4 @@ public record Crawling(
         @Nullable String selector,
         @Nullable List<Integer> indexes
 ) {
-
-    public Crawling {
-        if (CrawlingType.INDEX == type && (indexes == null || indexes.isEmpty())) {
-            throw new IllegalArgumentException(CrawlingType.INDEX + "타입은 indexes가 필수입니다.");
-        }
-        if (CrawlingType.SELECTOR == type && (selector == null || selector.isEmpty())) {
-            throw new IllegalArgumentException(CrawlingType.SELECTOR + "타입은 selector가 필수입니다.");
-        }
-    }
-
-    public static Crawling ofNone() {
-        return new Crawling(CrawlingType.NONE, null, null);
-    }
-
-    public static Crawling ofSelector(final String selector) {
-        return new Crawling(CrawlingType.SELECTOR, selector, null);
-    }
-
-    public static Crawling ofIndex(final List<Integer> indexes) {
-        return new Crawling(CrawlingType.INDEX, null, indexes);
-    }
 }
