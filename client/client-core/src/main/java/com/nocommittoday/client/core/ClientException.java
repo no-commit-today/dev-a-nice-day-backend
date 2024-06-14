019487df694d2c529079cfa624b8d6971079545a
Youngrae Cho <yrc9229@gmail.com>
2024-06-15 02:34:08 +0900

    feat(client): client-core 모듈 추가

---
+++ client/client-core/src/main/java/com/nocommittoday/client/core/ClientException.java
@@ -0,0 +1,8 @@
+package com.nocommittoday.client.core;
+
+public class ClientException extends RuntimeException {
+
+    public ClientException(final Throwable cause) {
+        super(cause);
+    }
+}
