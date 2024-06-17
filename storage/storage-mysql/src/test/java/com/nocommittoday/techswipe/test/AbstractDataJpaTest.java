3bf24692232fe9803632fc891b93d7d0de55a035
Youngrae Cho <yrc9229@gmail.com>
2024-06-17 20:29:07 +0900

    feat(subscription): SubscriptionListReader 구현

---
+++ storage/storage-mysql/src/test/java/com/nocommittoday/techswipe/test/AbstractDataJpaTest.java
@@ -0,0 +1,17 @@
+package com.nocommittoday.techswipe.test;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@Import(TestJpaConfig.class)
+@Transactional
+public abstract class AbstractDataJpaTest {
+
+
+    @Autowired
+    protected EntityManager em;
+}
