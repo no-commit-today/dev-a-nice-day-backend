d6f1e16bd0b44c6dfca5929fb1bec7d6b2185d40
Youngrae Cho <yrc9229@gmail.com>
2024-06-22 02:10:28 +0900

    fix(content): 컨텐츠 제공자 중복 등록 에러 처리

--- domain/content/content-application/src/test/java/com/nocommittoday/techswipe/content/service/ProviderRegisterServiceTest.java
+++ domain/content/content-application/src/test/java/com/nocommittoday/techswipe/content/service/ProviderRegisterServiceTest.java
@@ -3,7 +3,9 @@ package com.nocommittoday.techswipe.content.service;
 import com.nocommittoday.techswipe.content.domain.TechContentProvider;
 import com.nocommittoday.techswipe.content.domain.TechContentProviderCreate;
 import com.nocommittoday.techswipe.content.domain.TechContentProviderType;
+import com.nocommittoday.techswipe.content.domain.TechContentProviderUrlExistsException;
 import com.nocommittoday.techswipe.content.infrastructure.ProviderAppender;
+import com.nocommittoday.techswipe.content.infrastructure.ProviderUrlExistsReader;
 import com.nocommittoday.techswipe.image.domain.Image;
 import com.nocommittoday.techswipe.image.service.ImageStoreService;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +32,9 @@ class ProviderRegisterServiceTest {
     @Mock
     private ProviderAppender providerAppender;
 
+    @Mock
+    private ProviderUrlExistsReader providerUrlExistsReader;
+
     @Captor
     private ArgumentCaptor<TechContentProviderCreate> captor;
 
@@ -41,6 +47,8 @@ class ProviderRegisterServiceTest {
                 "url",
                 null
         );
+        given(providerUrlExistsReader.exists("url"))
+                .willReturn(false);
         given(providerAppender.save(captor.capture()))
                 .willReturn(new TechContentProvider.TechContentProviderId(1L));
 
@@ -64,6 +72,8 @@ class ProviderRegisterServiceTest {
                 "url",
                 "icon-url"
         );
+        given(providerUrlExistsReader.exists("url"))
+                .willReturn(false);
         given(imageStoreService.store("icon-url", "provider-icon"))
                 .willReturn(new Image.ImageId(1L));
         given(providerAppender.save(captor.capture()))
@@ -78,5 +88,22 @@ class ProviderRegisterServiceTest {
         assertThat(captor.getValue().title()).isEqualTo("title");
         assertThat(captor.getValue().url()).isEqualTo("url");
         assertThat(captor.getValue().iconId()).isEqualTo(new Image.ImageId(1L));
+    }
+
+    @Test
+    void 컨텐츠_제공자_url_이_이미_등록되어_있을_경우_예외를_발생시킨다() {
+        // given
+        final ProviderRegisterCommand command = new ProviderRegisterCommand(
+                TechContentProviderType.DOMESTIC_COMPANY_BLOG,
+                "title",
+                "url",
+                null
+        );
+        given(providerUrlExistsReader.exists("url"))
+                .willReturn(true);
+
+        // when & then
+        assertThatThrownBy(() -> providerRegisterService.register(command))
+                .isInstanceOf(TechContentProviderUrlExistsException.class);
     }
 }
