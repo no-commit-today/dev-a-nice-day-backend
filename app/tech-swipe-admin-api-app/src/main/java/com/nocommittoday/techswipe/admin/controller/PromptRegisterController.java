0453c59cb8d6652da018fad22c16ee74b0b578ac
Youngrae Cho <yrc9229@gmail.com>
2024-06-22 12:32:29 +0900

    fix(admin): 프롬프트 등록 api validated 추가

--- app/tech-swipe-admin-api-app/src/main/java/com/nocommittoday/techswipe/admin/controller/PromptRegisterController.java
+++ app/tech-swipe-admin-api-app/src/main/java/com/nocommittoday/techswipe/admin/controller/PromptRegisterController.java
@@ -3,6 +3,7 @@ package com.nocommittoday.techswipe.admin.controller;
 import com.nocommittoday.techswipe.collection.domain.Prompt;
 import com.nocommittoday.techswipe.collection.service.PromptRegisterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,9 @@ public class PromptRegisterController {
     private final PromptRegisterService promptRegisterService;
 
     @PostMapping("/api/collection/admin/prompts")
-    public Prompt.PromptId register(@RequestBody final PromptRegisterRequest request) {
+    public Prompt.PromptId register(
+            @RequestBody @Validated final PromptRegisterRequest request
+    ) {
         return promptRegisterService.register(request.toCommand());
     }
 }
