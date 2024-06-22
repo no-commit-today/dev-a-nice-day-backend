21adc29b35d3545fcaab0d2822435a03d74f150b
Youngrae Cho <yrc9229@gmail.com>
2024-06-22 16:56:58 +0900

    feat(collection): 수집된 컨텐츠 url 조회 추가

--- storage/storage-mysql/src/main/java/com/nocommittoday/techswipe/collection/storage/mysql/CollectedContentJpaRepository.java
+++ storage/storage-mysql/src/main/java/com/nocommittoday/techswipe/collection/storage/mysql/CollectedContentJpaRepository.java
@@ -2,5 +2,6 @@ package com.nocommittoday.techswipe.collection.storage.mysql;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CollectedContentJpaRepository extends JpaRepository<CollectedContentEntity, Long> {
+public interface CollectedContentJpaRepository
+        extends JpaRepository<CollectedContentEntity, Long>, CollectedContentJpaRepositoryCustom {
 }
