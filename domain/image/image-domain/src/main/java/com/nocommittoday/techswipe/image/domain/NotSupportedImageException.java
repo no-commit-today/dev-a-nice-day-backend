cb9f9e78c256dbc1eef0fefc26507e2aaa50a107
Youngrae Cho <yrc9229@gmail.com>
2024-06-26 20:40:34 +0900

    fix(image): 이미지 타입 유추 로직 수정
    - image/x-icon;charset=UTF-8 와 같이 여러 타입이 있을 수 있음

--- domain/image/image-domain/src/main/java/com/nocommittoday/techswipe/image/domain/NotSupportedImageException.java
+++ domain/image/image-domain/src/main/java/com/nocommittoday/techswipe/image/domain/NotSupportedImageException.java
@@ -4,7 +4,7 @@ import com.nocommittoday.techswipe.core.domain.exception.AbstractDomainException
 
 public class NotSupportedImageException extends AbstractDomainException {
 
-    public NotSupportedImageException(final String contentType) {
-        super(ImageErrorCode.NOT_SUPPORTED_IMAGE, "Content type=" + contentType);
+    public NotSupportedImageException(final String url) {
+        super(ImageErrorCode.NOT_SUPPORTED_IMAGE, "url=" + url);
     }
 }
