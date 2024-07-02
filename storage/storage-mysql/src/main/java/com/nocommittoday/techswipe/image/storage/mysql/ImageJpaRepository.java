20c15459f5d1b36b945697b52e99c4f739affa43
Youngrae Cho <yrc9229@gmail.com>
2024-07-02 16:10:40 +0900

    feat(image): image id validator 추가

--- storage/storage-mysql/src/main/java/com/nocommittoday/techswipe/image/storage/mysql/ImageJpaRepository.java
+++ storage/storage-mysql/src/main/java/com/nocommittoday/techswipe/image/storage/mysql/ImageJpaRepository.java
@@ -3,4 +3,6 @@ package com.nocommittoday.techswipe.image.storage.mysql;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageJpaRepository extends JpaRepository<ImageEntity, Long> {
+
+    boolean existsByIdAndDeletedIsFalse(Long id);
 }
