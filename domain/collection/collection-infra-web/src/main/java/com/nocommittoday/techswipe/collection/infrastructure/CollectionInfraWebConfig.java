a38f295bbce6c7c2cea0a71a3cb95f8c67820a8d
Youngrae Cho <yrc9229@gmail.com>
2024-07-05 18:49:59 +0900

    feat(collection): open ai 설정 변경

--- domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/CollectionInfraWebConfig.java
+++ domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/CollectionInfraWebConfig.java
@@ -2,32 +2,42 @@ package com.nocommittoday.techswipe.collection.infrastructure;
 
 import com.theokanning.openai.service.OpenAiService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
 public class CollectionInfraWebConfig {
 
-    @Bean
-    @Profile("local")
-    public CollectionProcessor localCollectionProcessor() {
-        return new LocalCollectionProcessor();
-    }
+    @Configuration
+    @ConditionalOnProperty(name = "app.collection.openai.enabled", havingValue = "true")
+    public static class OpenAi {
 
-    @Bean
-    @Profile("!local")
-    public CollectionProcessor openAiCollectionProcessor(
-            final OpenAiService openAiService
-    ) {
-        return new OpenAiCollectionProcessor(openAiService);
-    }
+        @Bean
+        public CollectionProcessor openAiCollectionProcessor(
+                final OpenAiService openAiService
+        ) {
+            return new OpenAiCollectionProcessor(openAiService);
+        }
 
-    @Bean
-    @Profile("!local")
-    public OpenAiService openAiService(
-            @Value("${app.client.openai.api-key}") final String apiKey
-    ) {
-        return new OpenAiService(apiKey);
+        @Bean
+        public OpenAiService openAiService(
+                @Value("${app.collection.openai.api-key}") final String apiKey
+        ) {
+            return new OpenAiService(apiKey);
+        }
+
+    }
+    @Configuration
+    @ConditionalOnMissingBean(OpenAi.class)
+    public static class Local {
+
+        @Bean
+        public CollectionProcessor localCollectionProcessor() {
+            return new LocalCollectionProcessor();
+        }
+
+    }
+    private CollectionInfraWebConfig() {
     }
 }
