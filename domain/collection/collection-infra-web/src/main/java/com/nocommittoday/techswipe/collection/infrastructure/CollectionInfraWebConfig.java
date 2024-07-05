12a6b8add5ea9ebe1f353aefe7601fbf10722254
Cho-D-YoungRae <49931252+Cho-D-YoungRae@users.noreply.github.com>
2024-07-05 19:01:52 +0900

    Feat/#71 프로필별 환경 세팅 (#72)
    * style(collection): CollectionWebConfig 이름 변경
    
    * feat(collection): open ai 설정 변경
    
    * chore(collection): 설정 예시 추가
    
    * chore(batch): prod openai 사용 추가
    
    * chore(app): 개발환경 s3 추가

---
+++ domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/CollectionInfraWebConfig.java
@@ -0,0 +1,43 @@
+package com.nocommittoday.techswipe.collection.infrastructure;
+
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class CollectionInfraWebConfig {
+
+    @Configuration
+    @ConditionalOnProperty(name = "app.collection.openai.enabled", havingValue = "true")
+    public static class OpenAi {
+
+        @Bean
+        public CollectionProcessor openAiCollectionProcessor(
+                final OpenAiService openAiService
+        ) {
+            return new OpenAiCollectionProcessor(openAiService);
+        }
+
+        @Bean
+        public OpenAiService openAiService(
+                @Value("${app.collection.openai.api-key}") final String apiKey
+        ) {
+            return new OpenAiService(apiKey);
+        }
+
+    }
+    @Configuration
+    @ConditionalOnMissingBean(OpenAi.class)
+    public static class Local {
+
+        @Bean
+        public CollectionProcessor localCollectionProcessor() {
+            return new LocalCollectionProcessor();
+        }
+
+    }
+    private CollectionInfraWebConfig() {
+    }
+}
