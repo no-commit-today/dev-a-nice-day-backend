49cbf79e8385e49e384a4a73cbdbfc978f7eb498
Youngrae Cho <yrc9229@gmail.com>
2024-07-07 05:22:23 +0900

    feat(batch): from, to jobparameter 추가

---
+++ app/tech-swipe-batch-app/src/main/java/com/nocommittoday/techswipe/batch/param/LocalDateTimeFromJobParameter.java
@@ -0,0 +1,21 @@
+package com.nocommittoday.techswipe.batch.param;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeFromJobParameter {
+
+    public static final String NAME = "LocalDateTimeFromJobParameter";
+
+    private LocalDateTime from;
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    @Value("#{jobParameters['from']}")
+    public void setFrom(final LocalDateTime from) {
+        this.from = from;
+    }
+}
---
+++ app/tech-swipe-batch-app/src/main/java/com/nocommittoday/techswipe/batch/param/LocalDateTimeToJobParameter.java
@@ -0,0 +1,21 @@
+package com.nocommittoday.techswipe.batch.param;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeToJobParameter {
+
+    public static final String NAME = "LocalDateTimeToJobParameter";
+
+    private LocalDateTime to;
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+
+    @Value("#{jobParameters['to']}")
+    public void setTo(final LocalDateTime to) {
+        this.to = to;
+    }
+}
