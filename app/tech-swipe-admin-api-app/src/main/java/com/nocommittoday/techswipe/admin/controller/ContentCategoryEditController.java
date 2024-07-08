2cb6f3e2bef20ffbde19bb5253608c44b610934d
Youngrae Cho <yrc9229@gmail.com>
2024-07-08 19:09:57 +0900

    feat(admin): 컬렉션 카테고리 컨텐츠 반영 API 추가

--- app/tech-swipe-admin-api-app/src/main/java/com/nocommittoday/techswipe/admin/controller/ContentCategoryEditController.java
+++ app/tech-swipe-admin-api-app/src/main/java/com/nocommittoday/techswipe/admin/controller/ContentCategoryEditController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,12 @@ public class ContentCategoryEditController {
                 new CollectedContent.Id(collectionId),
                 new ContentCategoryEdit(request.categories())
         );
+    }
+
+    @PostMapping("/api/collection/admin/collections/{collectionId}/apply-categories")
+    public void apply(
+            @PathVariable final Long collectionId
+    ) {
+        categoryEditService.applyCategoryEdited(new CollectedContent.Id(collectionId));
     }
 }
