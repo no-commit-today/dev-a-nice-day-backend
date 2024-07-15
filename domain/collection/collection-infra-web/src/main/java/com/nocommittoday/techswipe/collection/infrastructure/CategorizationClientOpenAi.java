de0c86d130a490a97c5e7abcb5882468f240408d
Youngrae Cho <yrc9229@gmail.com>
2024-07-15 15:56:08 +0900

    feat(collection): 프롬프트 분리
    - job 에서 필요

--- domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/CategorizationClientOpenAi.java
+++ domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/CategorizationClientOpenAi.java
@@ -1,32 +1,12 @@
 package com.nocommittoday.techswipe.collection.infrastructure;
 
 import com.nocommittoday.techswipe.collection.domain.CollectedContent;
-import com.nocommittoday.techswipe.collection.domain.CollectionCategory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatOptions;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 @Slf4j
 class CategorizationClientOpenAi implements CategorizationClient {
-
-    public static final String PROMPT = String.format("""
-                    - 당신은 분류를 정말 잘 하는 봇입니다.
-                    - 카테고리는 [%s] 중 선택합니다.
-                    - 카테고리는 최소 1개에서 최대 3개입니다.
-                    - 아래 [답변 형식]에 맞게 답변해야 합니다.
-                    - 지시한 내용들을 지키지 못하면 당신은 불이익을 받을 것입니다.
-                    
-                    [답변 형식]
-                    - ...
-                    - ...
-                    """,
-            Arrays.stream(CollectionCategory.values())
-                    .map(CollectionCategory::name)
-                    .collect(Collectors.joining(","))
-    );
 
     private final ChatClient chatClient;
 
@@ -45,7 +25,7 @@ class CategorizationClientOpenAi implements CategorizationClient {
     public String categorize(final CollectedContent collectedContent) {
         return chatClient.prompt()
                 .advisors(new CategorizationLoggerAdvisor(collectedContent))
-                .system(PROMPT)
+                .system(CategorizationConst.PROMPT)
                 .user(collectedContent.getTitle() + "\n\n" + collectedContent.getContent())
                 .call()
                 .content()
