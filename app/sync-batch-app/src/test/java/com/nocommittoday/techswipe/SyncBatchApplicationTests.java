570f1366d5cf0851424a98e8ca155918c0337233
Youngrae Cho <yrc9229@gmail.com>
2024-07-15 23:05:18 +0900

    chore(batch-app): sync batch 분리

---
+++ app/sync-batch-app/src/test/java/com/nocommittoday/techswipe/SyncBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocommittoday.techswipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
