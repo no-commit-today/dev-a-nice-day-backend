e609e375b8880cae1dae039f939dcf8846a03bbb
Youngrae Cho <yrc9229@gmail.com>
2024-07-16 18:08:34 +0900

    feat(collection): SummarizationValidator 추가

--- domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/SummarizationProcessor.java
+++ domain/collection/collection-infra-web/src/main/java/com/nocommittoday/techswipe/collection/infrastructure/SummarizationProcessor.java
@@ -4,33 +4,21 @@ import com.nocommittoday.techswipe.collection.domain.CollectedContent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.regex.Pattern;
-
 @Component
 @RequiredArgsConstructor
 public class SummarizationProcessor {
 
-    private static final Pattern SUMMARIZATION_RESULT_PATTERN = Pattern.compile("""
-            ^1\\. [^\\n]+
-            2\\. [^\\n]+
-            3\\. [^\\n]+$""");
-
-    private static final Pattern KOREAN_CONTAINED_PATTERN = Pattern.compile("[가-힣]");
-
     private final SummarizationClient summarizationClient;
+
+    private final SummarizationValidator summarizationValidator;
 
     public SummarizationResult summarize(final CollectedContent collectedContent) {
         try {
             final String responseContent = summarizationClient.summarize(collectedContent);
 
-            if (!SUMMARIZATION_RESULT_PATTERN.matcher(responseContent).matches()) {
+            if (!summarizationValidator.check(responseContent)) {
                 throw new SummarizationResponseInvalidException(
                         "요약 결과가 올바르지 않습니다. ", collectedContent.getId(), responseContent
-                );
-            }
-            if (!KOREAN_CONTAINED_PATTERN.matcher(responseContent).find()) {
-                throw new SummarizationResponseInvalidException(
-                        "한글 요약 결과가 아닙니다. ", collectedContent.getId(), responseContent
                 );
             }
 
