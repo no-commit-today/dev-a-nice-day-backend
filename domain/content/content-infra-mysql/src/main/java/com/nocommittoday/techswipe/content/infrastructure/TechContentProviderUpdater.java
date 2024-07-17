8c6ea5d42c9a074377f9d96a02571793bc87bf82
Youngrae Cho <yrc9229@gmail.com>
2024-07-17 23:01:32 +0900

    feat(storage-mysql): 제공자 아이콘 디비 변경

---
+++ domain/content/content-infra-mysql/src/main/java/com/nocommittoday/techswipe/content/infrastructure/TechContentProviderUpdater.java
@@ -0,0 +1,23 @@
+package com.nocommittoday.techswipe.content.infrastructure;
+
+import com.nocommittoday.techswipe.content.domain.TechContentProvider;
+import com.nocommittoday.techswipe.content.domain.TechContentProviderNotFoundException;
+import com.nocommittoday.techswipe.content.storage.mysql.TechContentProviderEntity;
+import com.nocommittoday.techswipe.content.storage.mysql.TechContentProviderJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@RequiredArgsConstructor
+public class TechContentProviderUpdater {
+
+    private final TechContentProviderJpaRepository techContentProviderJpaRepository;
+
+    @Transactional
+    public void update(final TechContentProvider provider) {
+        final TechContentProviderEntity entity = techContentProviderJpaRepository.findById(provider.getId().value())
+                .orElseThrow(() -> new TechContentProviderNotFoundException(provider.getId()));
+        entity.update(provider);
+    }
+}
