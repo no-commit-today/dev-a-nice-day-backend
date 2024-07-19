a2b519f1e28fbb4cf7c15346b3f21a59d1b59f05
Youngrae Cho <yrc9229@gmail.com>
2024-07-20 01:06:05 +0900

    feat(batch): healthCheckJob 추가

---
+++ app/batch-app-core/src/main/java/com/nocommittoday/techswipe/batch/job/HealthCheckJobConfig.java
@@ -0,0 +1,51 @@
+package com.nocommittoday.techswipe.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.builder.TaskletStepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class HealthCheckJobConfig {
+
+    private static final String JOB_NAME = "healthCheckJob";
+    private static final String STEP_NAME = "healthCheckStep";
+
+    private final JobRepository jobRepository;
+
+    private final PlatformTransactionManager txManager;
+
+    @Bean(JOB_NAME)
+    public Job job() {
+        final JobBuilder jobBuilder = new JobBuilder(JOB_NAME, jobRepository);
+        return jobBuilder
+                .incrementer(new RunIdIncrementer())
+                .start(step())
+                .build();
+    }
+
+    @Bean(STEP_NAME)
+    @JobScope
+    public Step step() {
+        final TaskletStepBuilder taskletStepBuilder = new TaskletStepBuilder(new StepBuilder(STEP_NAME, jobRepository));
+        return taskletStepBuilder
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("<<<<<<<<<<<< 헬스체크 >>>>>>>>>>>>");
+                    return RepeatStatus.FINISHED;
+                }, txManager)
+                .build();
+    }
+
+}
