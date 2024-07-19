5c889a7b5d1aca1b30eeab8d736b70eb44010263
Youngrae Cho <yrc9229@gmail.com>
2024-07-20 01:07:08 +0900

    feat(batch-app): 웹 어플리케이션으로 안뜨도록 수정

--- app/tech-swipe-batch-app/src/main/java/com/nocommittoday/techswipe/TechSwipeBatchApplication.java
+++ app/tech-swipe-batch-app/src/main/java/com/nocommittoday/techswipe/TechSwipeBatchApplication.java
@@ -1,12 +1,15 @@
 package com.nocommittoday.techswipe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechSwipeBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TechSwipeBatchApplication.class, args);
+        final SpringApplication app = new SpringApplication(TechSwipeBatchApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 }
