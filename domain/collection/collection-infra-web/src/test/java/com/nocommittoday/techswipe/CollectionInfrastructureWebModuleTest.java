f1ba3df1c38f45be94bc75921ee62e5d5e5bbd32
Youngrae Cho <yrc9229@gmail.com>
2024-07-20 01:05:42 +0900

    test(collection): 인프라 웹 contextload 테스트 깨짐 수정

--- domain/collection/collection-infra-web/src/test/java/com/nocommittoday/techswipe/CollectionInfrastructureWebModuleTest.java
+++ domain/collection/collection-infra-web/src/test/java/com/nocommittoday/techswipe/CollectionInfrastructureWebModuleTest.java
@@ -2,13 +2,29 @@ package com.nocommittoday.techswipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestClient;
 
 @SpringBootTest
 @SpringBootApplication
+@Import(CollectionInfrastructureWebModuleTest.Config.class)
 class CollectionInfrastructureWebModuleTest {
 
     @Test
     void contextLoad() {
+    }
+
+    static class Config {
+
+        @Bean
+        @Scope("prototype")
+        @ConditionalOnMissingBean
+        public RestClient.Builder restClientBuilder() {
+            return RestClient.builder();
+        }
     }
 }
