674cddc9cd6014247c374a4fafbadb290e6c32f4
Youngrae Cho <yrc9229@gmail.com>
2024-07-22 21:59:47 +0900

    style(sync-batch-app): 동기화 배치 앱 이름 변경

--- app/sync-batch-app/src/main/java/com/nocommittoday/techswipe/TechSwipeSyncBatchApplication.java
+++ app/sync-batch-app/src/main/java/com/nocommittoday/techswipe/TechSwipeSyncBatchApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SyncBatchApplication {
+public class TechSwipeSyncBatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SyncBatchApplication.class, args);
+        SpringApplication.run(TechSwipeSyncBatchApplication.class, args);
     }
 }
