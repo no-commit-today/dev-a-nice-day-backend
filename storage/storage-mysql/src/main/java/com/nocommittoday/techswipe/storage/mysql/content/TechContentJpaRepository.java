57d0c3f0ea6217a02962b6b7973acec0786e4c96
Youngrae Cho <yrc9229@gmail.com>
2024-08-04 01:21:20 +0900

    prune(storage-mysql): TechContentJpaRepository 불필요한 메서드 제거

--- storage/storage-mysql/src/main/java/com/nocommittoday/techswipe/storage/mysql/content/TechContentJpaRepository.java
+++ storage/storage-mysql/src/main/java/com/nocommittoday/techswipe/storage/mysql/content/TechContentJpaRepository.java
@@ -1,14 +1,7 @@
 package com.nocommittoday.techswipe.storage.mysql.content;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public interface TechContentJpaRepository
         extends JpaRepository<TechContentEntity, Long>, TechContentJpaRepositoryCustom {
-
-    List<TechContentEntity> findAllByLastModifiedAtGreaterThanEqualAndLastModifiedAtLessThan(
-            Pageable pageable, LocalDateTime lastModifiedAtStart, LocalDateTime lastModifiedAtEnd);
 }
