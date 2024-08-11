4d16bb8b8a78fe855a7cf56fcfb272b01bd7fe6d
Youngrae Cho <yrc9229@gmail.com>
2024-08-11 19:35:47 +0900

    feat(user): LogoutProcessor 구현

--- domain/domain-application/src/main/java/com/nocommittoday/techswipe/domain/user/LogoutProcessor.java
+++ domain/domain-application/src/main/java/com/nocommittoday/techswipe/domain/user/LogoutProcessor.java
@@ -1,11 +1,23 @@
 package com.nocommittoday.techswipe.domain.user;
 
+import com.nocommittoday.techswipe.storage.mysql.user.LoggedInEntity;
+import com.nocommittoday.techswipe.storage.mysql.user.LoggedInEntityJpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class LogoutProcessor {
 
-    public void process(LoggedInUser user) {
+    private final LoggedInEntityJpaRepository loggedInEntityJpaRepository;
 
+    public LogoutProcessor(LoggedInEntityJpaRepository loggedInEntityJpaRepository) {
+        this.loggedInEntityJpaRepository = loggedInEntityJpaRepository;
+    }
+
+    @Transactional
+    public void process(LoggedInUser user) {
+        loggedInEntityJpaRepository.findByRefreshTokenId(
+                user.getLoggedIn().getRefreshTokenId().value().toString()
+        ).ifPresent(LoggedInEntity::delete);
     }
 }
