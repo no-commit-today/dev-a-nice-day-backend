9b7b919d566f65df1c244f8e88608325a2a8124e
Youngrae Cho <yrc9229@gmail.com>
2024-08-14 18:11:34 +0900

    fix: 도커파일 및 문서 접근 필터 수정

--- app/techswipe-api-app/src/main/java/com/nocommittoday/techswipe/config/SpringRestDocsAccessFilter.java
+++ app/techswipe-api-app/src/main/java/com/nocommittoday/techswipe/config/SpringRestDocsAccessFilter.java
@@ -6,21 +6,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 /**
  * 참고: https://taetaetae.github.io/2020/03/08/spring-rest-docs-in-spring-boot/#springprofile-에-따라-spring-rest-docs-url-을-접근-가능불가능-할-수-있게-한다
  */
-@Profile("prod")
-@Component
-@WebFilter(urlPatterns = "/docs/**")
+//@Profile("prod")
+//@WebFilter(urlPatterns = "/docs/**")
 public class SpringRestDocsAccessFilter implements Filter {
 
     private static final Logger log = LoggerFactory.getLogger(SpringRestDocsAccessFilter.class);
