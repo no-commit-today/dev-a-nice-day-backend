6b6baeb79974227f1abd7bafaeb76458fe208a9b
Youngrae Cho <yrc9229@gmail.com>
2024-08-27 22:24:55 +0900

    chore: api 앱 이름 수정

--- app/api-app/src/main/java/com/nocommittoday/techswipe/ApiApplication.java
+++ app/api-app/src/main/java/com/nocommittoday/techswipe/ApiApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechSwipeApiApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TechSwipeApiApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
