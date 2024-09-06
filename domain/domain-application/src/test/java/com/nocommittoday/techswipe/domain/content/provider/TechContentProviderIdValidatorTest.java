2de3c6113f95e9b896d39a5fc6fba89ff358ca38
Youngrae Cho <yrc9229@gmail.com>
2024-09-06 15:20:42 +0900

    feat(domain): TechContentProviderIdValidator 예외 추가

--- domain/domain-application/src/test/java/com/nocommittoday/techswipe/domain/content/provider/TechContentProviderIdValidatorTest.java
+++ domain/domain-application/src/test/java/com/nocommittoday/techswipe/domain/content/provider/TechContentProviderIdValidatorTest.java
@@ -1,7 +1,8 @@
 package com.nocommittoday.techswipe.domain.content.provider;
 
 import com.nocommittoday.techswipe.domain.content.TechContentProviderId;
-import com.nocommittoday.techswipe.domain.content.exception.TechContentProviderNotFoundException;
+import com.nocommittoday.techswipe.domain.core.DomainValidationException;
+import com.nocommittoday.techswipe.storage.mysql.content.TechContentProviderJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,17 +19,18 @@ class TechContentProviderIdValidatorTest {
     private TechContentProviderIdValidator techContentProviderIdValidator;
 
     @Mock
-    private TechContentProviderExistsReader techContentProviderExistsReader;
+    private TechContentProviderJpaRepository techContentProviderJpaRepository;
 
     @Test
     void 컨텐츠_제공자가_존재하지_않을_경우_예외를_발생시킨다() {
         // given
         TechContentProviderId id = new TechContentProviderId(1L);
-        given(techContentProviderExistsReader.exists(id)).willReturn(false);
+        given(techContentProviderJpaRepository.existsByIdAndDeletedIsFalse(id.value()))
+                .willReturn(false);
 
         // when
         // then
         assertThatThrownBy(() -> techContentProviderIdValidator.validate(id))
-                .isInstanceOf(TechContentProviderNotFoundException.class);
+                .isInstanceOf(DomainValidationException.class);
     }
 }
