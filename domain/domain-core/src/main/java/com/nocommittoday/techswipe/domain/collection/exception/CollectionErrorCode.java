f8e599c41e5f5f4ce392e8dd6b8aa47ea9bcddcd
Youngrae Cho <yrc9229@gmail.com>
2024-09-06 17:34:12 +0900

    prune(domain): 불필요한 컬렉션 도메인 예외 제거

--- domain/domain-core/src/main/java/com/nocommittoday/techswipe/domain/collection/exception/CollectionErrorCode.java
+++ domain/domain-core/src/main/java/com/nocommittoday/techswipe/domain/collection/exception/CollectionErrorCode.java
@@ -9,10 +9,6 @@ public enum CollectionErrorCode implements ErrorCodeType {
     SUMMARIZE_UNABLE("요약할 수 없습니다.", HttpURLConnection.HTTP_BAD_REQUEST),
     NOT_FOUND("수집된 컨텐츠를 찾을 수 없습니다.", HttpURLConnection.HTTP_NOT_FOUND),
     PUBLISH_UNABLE("컨텐츠를 발행할 수 없습니다.", HttpURLConnection.HTTP_BAD_REQUEST),
-    CATEGORY_NOT_EDITABLE("카테고리를 수정할 수 없습니다.", HttpURLConnection.HTTP_BAD_REQUEST),
-    CATEGORY_NOT_APPLICABLE("카테고리를 컨텐츠에 반영할 수 없습니다.", HttpURLConnection.HTTP_BAD_REQUEST),
-    ILLEGAL_PROVIDER_ID("잘못된 컨텐츠 제공자 아이디입니다.", HttpURLConnection.HTTP_BAD_REQUEST),
-    ALREADY_COLLECTED("이미 수집된 컨텐츠입니다.", HttpURLConnection.HTTP_BAD_REQUEST),
     ILLEGAL_CATEGORY("잘못된 카테고리입니다.", HttpURLConnection.HTTP_BAD_REQUEST),
     INITIALIZATION_FAILURE("초기화에 실패했습니다.", HttpURLConnection.HTTP_BAD_REQUEST),
     ;
