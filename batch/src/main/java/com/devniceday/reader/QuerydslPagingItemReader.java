30b2655d8a8b4464a666f19a6642b7eaf955227c
Youngrae Cho <yrc9229@gmail.com>
2024-11-12 21:56:44 +0900

    feat(batch): reader, writer 이동

---
+++ batch/src/main/java/com/devniceday/reader/QuerydslPagingItemReader.java
@@ -0,0 +1,117 @@
+package com.devniceday.reader;
+
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.springframework.batch.item.database.AbstractPagingItemReader;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Function;
+
+/**
+ * https://github.com/jojoldu/spring-batch-querydsl
+ */
+public class QuerydslPagingItemReader<T> extends AbstractPagingItemReader<T> {
+
+    protected final Map<String, Object> jpaPropertyMap = new HashMap<>();
+    protected EntityManagerFactory entityManagerFactory;
+    protected EntityManager entityManager;
+    protected Function<JPAQueryFactory, JPAQuery<T>> queryFunction;
+    protected boolean transacted = true;
+
+    public QuerydslPagingItemReader() {
+        setName(ClassUtils.getShortName(QuerydslPagingItemReader.class));
+    }
+
+    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public void setQueryFunction(Function<JPAQueryFactory, JPAQuery<T>> queryFunction) {
+        this.queryFunction = queryFunction;
+    }
+
+    public void setTransacted(boolean transacted) {
+        this.transacted = transacted;
+    }
+
+    @Override
+    protected void doOpen() throws Exception {
+        super.doOpen();
+
+        entityManager = entityManagerFactory.createEntityManager(jpaPropertyMap);
+        if (entityManager == null) {
+            throw new DataAccessResourceFailureException("Unable to obtain an EntityManager");
+        }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void doReadPage() {
+        EntityTransaction tx = getTxOrNull();
+
+        JPQLQuery<T> query = createQuery()
+                .offset((long) getPage() * getPageSize())
+                .limit(getPageSize());
+
+        initResults();
+
+        fetchQuery(query, tx);
+    }
+
+    protected EntityTransaction getTxOrNull() {
+        if (transacted) {
+            EntityTransaction tx = entityManager.getTransaction();
+            tx.begin();
+
+            entityManager.flush();
+            entityManager.clear();
+            return tx;
+        }
+
+        return null;
+    }
+
+    protected JPAQuery<T> createQuery() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+        return queryFunction.apply(queryFactory);
+    }
+
+    protected void initResults() {
+        if (CollectionUtils.isEmpty(results)) {
+            results = new CopyOnWriteArrayList<>();
+        } else {
+            results.clear();
+        }
+    }
+
+    protected void fetchQuery(JPQLQuery<T> query, EntityTransaction tx) {
+        if (transacted) {
+            results.addAll(query.fetch());
+            if(tx != null) {
+                tx.commit();
+            }
+        } else {
+            List<T> queryResult = query.fetch();
+            for (T entity : queryResult) {
+                entityManager.detach(entity);
+                results.add(entity);
+            }
+        }
+    }
+
+    @Override
+    protected void doClose() throws Exception {
+        entityManager.close();
+        super.doClose();
+    }
+}
