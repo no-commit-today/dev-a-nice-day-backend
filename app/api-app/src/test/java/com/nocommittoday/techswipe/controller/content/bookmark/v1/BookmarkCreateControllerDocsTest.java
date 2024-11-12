8f27087f725de98d84f25ebbc7be894f7be2f9cc
Youngrae Cho <yrc9229@gmail.com>
2024-11-12 21:21:48 +0900

    docs(domain): 북마크 중복 생성시 스킵 로직 추가

--- app/api-app/src/test/java/com/nocommittoday/techswipe/controller/content/bookmark/v1/BookmarkCreateControllerDocsTest.java
+++ app/api-app/src/test/java/com/nocommittoday/techswipe/controller/content/bookmark/v1/BookmarkCreateControllerDocsTest.java
@@ -1,6 +1,5 @@
 package com.nocommittoday.techswipe.controller.content.bookmark.v1;
 
-import com.nocommittoday.techswipe.controller.content.bookmark.v1.BookmarkCreateController;
 import com.nocommittoday.techswipe.controller.content.bookmark.v1.request.BookmarkCreateRequest;
 import com.nocommittoday.techswipe.docs.restdocs.AbstractDocsTest;
 import com.nocommittoday.techswipe.domain.content.bookmark.BookmarkCreateService;
@@ -17,8 +16,11 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookmarkCreateController.class)
@@ -34,7 +36,7 @@ class BookmarkCreateControllerDocsTest extends AbstractDocsTest {
     }
 
     @Test
-    void 북마크_생성_Docs() throws Exception {
+    void 북마크_생성_Old_Docs() throws Exception {
         // given
         BookmarkCreateRequest request = new BookmarkCreateRequest(
                 "프론트",
@@ -48,13 +50,32 @@ class BookmarkCreateControllerDocsTest extends AbstractDocsTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
-                .andDo(document("bookmark/create",
+                .andDo(document("bookmark/create-old",
                         requestHeaders(
                                 headerWithName(HttpHeaders.AUTHORIZATION).description("인증 필요")
                         ),
                         requestFields(
                                 fieldWithPath("groupName").description("그룹 이름"),
                                 fieldWithPath("contentId").description("컨텐츠 ID")
+                        )
+                ));
+    }
+
+    @Test
+    void 북마크_생성() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(put("/api/bookmark/v1/groups/{groupName}/contents/{contentId}", "프론트", 1L)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer access-token"))
+                .andExpect(status().isOk())
+                .andDo(document("bookmark/create",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("인증 필요")
+                        ),
+                        pathParameters(
+                                parameterWithName("groupName").description("그룹 이름"),
+                                parameterWithName("contentId").description("컨텐츠 ID")
                         )
                 ));
     }
