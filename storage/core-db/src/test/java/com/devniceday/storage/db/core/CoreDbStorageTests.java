b819f9a09ff0dd724cf05b80b451cff97c1d2ffc
Youngrae Cho <yrc9229@gmail.com>
2024-11-13 20:28:45 +0900

    chore(core-db): core-db 모듈 추가

---
+++ storage/core-db/src/test/java/com/devniceday/storage/db/core/CoreDbStorageTests.java
@@ -0,0 +1,16 @@
+package com.devniceday.storage.db.core;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("context")
+@SpringBootApplication
+@SpringBootTest
+class CoreDbStorageTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
