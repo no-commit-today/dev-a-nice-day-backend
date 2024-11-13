e4c8467d45ab554db32e789fc31454b13f9bb44f
Youngrae Cho <yrc9229@gmail.com>
2024-11-13 20:28:45 +0900

    chore(core-db): core-db 모듈 추가

---
+++ storage/core-db/src/testFixtures/java/com/nocommittoday/techswipe/storage/mysql/test/CleanUp.java
@@ -0,0 +1,26 @@
+package com.nocommittoday.techswipe.storage.mysql.test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Table;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CleanUp {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final EntityManager entityManager;
+
+    public CleanUp(JdbcTemplate jdbcTemplate, EntityManager entityManager) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    public void all() {
+        entityManager.getMetamodel().getEntities().forEach(entityType -> {
+            String tableName = entityType.getJavaType().getAnnotation(Table.class).name();
+            jdbcTemplate.execute("TRUNCATE TABLE " + tableName);
+        });
+    }
+}
