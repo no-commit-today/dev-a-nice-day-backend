38e207899089e84075df9c4d3d76235e3e3d8ae8
Youngrae Cho <yrc9229@gmail.com>
2024-11-14 11:44:03 +0900

    test(batch): 테스트 세팅

---
+++ batch/src/test/java/com/devniceday/BatchApplicationTest.java
@@ -0,0 +1,14 @@
+package com.devniceday;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Tag("context")
+@SpringBootTest
+class BatchApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
