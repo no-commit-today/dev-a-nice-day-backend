8766ef13936a7c98b76ca11e0a0dcdd3dc815703
Youngrae Cho <yrc9229@gmail.com>
2024-11-14 12:03:38 +0900

    feat(batch): id-generator 모듈 추가

--- batch/src/main/java/com/devniceday/BatchApplication.java
+++ batch/src/main/java/com/devniceday/BatchApplication.java
@@ -3,6 +3,10 @@ package com.devniceday;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
+import java.time.ZoneId;
 
 @SpringBootApplication
 public class BatchApplication {
@@ -11,5 +15,10 @@ public class BatchApplication {
         SpringApplication app = new SpringApplication(BatchApplication.class);
         app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.system(ZoneId.of("Asia/Seoul"));
     }
 }
