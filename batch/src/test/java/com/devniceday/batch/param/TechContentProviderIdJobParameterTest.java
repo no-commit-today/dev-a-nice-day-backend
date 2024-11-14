a761bf07eba2aa3cc86920ea44a3c57cb281c714
Youngrae Cho <yrc9229@gmail.com>
2024-11-14 12:07:51 +0900

    test(batch): TechContentProviderIdJobParameter

---
+++ batch/src/test/java/com/devniceday/batch/param/TechContentProviderIdJobParameterTest.java
@@ -0,0 +1,29 @@
+package com.devniceday.batch.param;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TechContentProviderIdJobParameterTest {
+
+    @Test
+    void 제공자ID_정상설정() {
+        TechContentProviderIdJobParameter param = new TechContentProviderIdJobParameter();
+        param.setProviderId(123L);
+        assertThat(param.getProviderId()).isEqualTo(123L);
+    }
+
+    @Test
+    void 제공자ID_설정되지않았을때_null반환() {
+        TechContentProviderIdJobParameter param = new TechContentProviderIdJobParameter();
+        assertThat(param.getProviderId()).isNull();
+    }
+
+    @Test
+    void 제공자ID_null값설정() {
+        TechContentProviderIdJobParameter param = new TechContentProviderIdJobParameter();
+        param.setProviderId(null);
+        assertThat(param.getProviderId()).isNull();
+    }
+
+}
