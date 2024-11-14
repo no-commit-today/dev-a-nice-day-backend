4984f224f59205c34fa3cc6808339a57bc7fd10d
Youngrae Cho <yrc9229@gmail.com>
2024-11-14 10:31:30 +0900

    feat(core-db): 컨텐츠 관련 엔티티 추가

---
+++ storage/core-db/src/main/java/com/devniceday/storage/db/core/TechContentUserCategoryJpaRepository.java
@@ -0,0 +1,6 @@
+package com.devniceday.storage.db.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TechContentUserCategoryJpaRepository extends JpaRepository<TechContentUserCategoryEntity, Long> {
+}
