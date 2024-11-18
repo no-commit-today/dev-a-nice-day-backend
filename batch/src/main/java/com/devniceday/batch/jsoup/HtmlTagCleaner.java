544b2c8a6139b4a290ecab91dc78aa4dd4197ec1
Youngrae Cho <yrc9229@gmail.com>
2024-11-18 12:41:58 +0900

    feat(batch): jsoup 기능 추가

---
+++ batch/src/main/java/com/devniceday/batch/jsoup/HtmlTagCleaner.java
@@ -0,0 +1,51 @@
+package com.devniceday.batch.jsoup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.NodeVisitor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HtmlTagCleaner {
+
+    public String clean(String html) {
+        Document document = Jsoup.parse(html);
+        Element element = document.body();
+        element.select("style").remove();
+
+        StringBuilder sb = new StringBuilder();
+        element.traverse(new TagCleanVisitor(sb));
+
+        return sb.toString();
+    }
+
+    private static class TagCleanVisitor implements NodeVisitor {
+
+        private final StringBuilder text;
+
+        public TagCleanVisitor(StringBuilder text) {
+            this.text = text;
+        }
+
+        public void head(Node node, int depth) {
+            if (node instanceof Element element) {
+                if (element.tag().isBlock() && !text.isEmpty()) {
+                    text.append("\n");
+                }
+            }
+        }
+
+        public void tail(Node node, int depth) {
+            if (node instanceof Element element) {
+                if (element.tag().isBlock() && !"br".equals(element.tagName())) {
+                    text.append("\n");
+                }
+            } else if (node instanceof TextNode textNode) {
+                text.append(textNode.getWholeText());
+            }
+        }
+    }
+}
