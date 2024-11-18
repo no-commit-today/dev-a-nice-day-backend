44e2b8d2021857b9b0305a836418be1903a9b18b
Youngrae Cho <yrc9229@gmail.com>
2024-11-18 14:15:03 +0900

    feat(batch): ContentCollectJob 추가

---
+++ batch/src/main/java/com/devniceday/batch/job/ContentCollectJobItemProcessor.java
@@ -0,0 +1,51 @@
+package com.devniceday.batch.job;
+
+import com.devniceday.batch.domain.CollectionStatus;
+import com.devniceday.batch.domain.ContentSubscriber;
+import com.devniceday.batch.domain.SubscribedContent;
+import com.devniceday.batch.domain.Subscription;
+import com.devniceday.module.idgenerator.IdGenerator;
+import com.devniceday.storage.db.core.BatchCollectedContentEntity;
+import com.devniceday.storage.db.core.BatchSubscriptionEntity;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.util.List;
+
+public class ContentCollectJobItemProcessor
+        implements ItemProcessor<BatchSubscriptionEntity, List<BatchCollectedContentEntity>> {
+
+    private final IdGenerator idGenerator;
+    private final SubscriptionMapper subscriptionMapper;
+    private final ContentSubscriber contentSubscriber;
+
+    public ContentCollectJobItemProcessor(
+            IdGenerator idGenerator,
+            SubscriptionMapper subscriptionMapper,
+            ContentSubscriber contentSubscriber
+    ) {
+        this.idGenerator = idGenerator;
+        this.subscriptionMapper = subscriptionMapper;
+        this.contentSubscriber = contentSubscriber;
+    }
+
+    @Override
+    public List<BatchCollectedContentEntity> process(BatchSubscriptionEntity item) throws Exception {
+        Subscription subscription = subscriptionMapper.map(item);
+        List<SubscribedContent> subscribedContents = contentSubscriber.subscribe(subscription);
+        return subscribedContents.stream()
+                .map(content -> new BatchCollectedContentEntity(
+                                idGenerator.nextId(),
+                                CollectionStatus.COLLECTED,
+                                subscription.providerId(),
+                                subscription.id(),
+                                content.url(),
+                                content.title(),
+                                content.publishedDate(),
+                                content.content(),
+                                content.imageUrl()
+                        )
+                )
+                .toList();
+    }
+
+}
