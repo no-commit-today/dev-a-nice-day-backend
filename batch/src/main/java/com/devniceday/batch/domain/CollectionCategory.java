c02fb012f1268794fed17372b87a4048ed694583
Youngrae Cho <yrc9229@gmail.com>
2024-11-25 22:06:14 +0900

    feat(batch): CollectionCategory 에 생성 로직 추가

--- batch/src/main/java/com/devniceday/batch/domain/CollectionCategory.java
+++ batch/src/main/java/com/devniceday/batch/domain/CollectionCategory.java
@@ -2,8 +2,16 @@ package com.devniceday.batch.domain;
 
 import com.devniceday.core.enums.EnumMapperType;
 import com.devniceday.core.enums.TechCategory;
+import jakarta.annotation.Nullable;
 
-import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public enum CollectionCategory implements EnumMapperType {
 
@@ -49,4 +57,36 @@ public enum CollectionCategory implements EnumMapperType {
         return techCategory != null;
     }
 
+    public static final int MIN_LIST_SIZE = 1;
+    public static final int MAX_LIST_SIZE = 3;
+
+    private static final String PATTERN_CATEGORY_GROUP_NAME = "category";
+    private static final Pattern CHAT_COMPLETION_RESULT_PATTERN = Pattern.compile("^- (" +
+            "?<" + PATTERN_CATEGORY_GROUP_NAME + ">"
+            + Arrays.stream(CollectionCategory.values())
+            .map(CollectionCategory::name)
+            .collect(Collectors.joining("|"))
+            + ")$");
+
+    @Nullable
+    public static List<CollectionCategory> createListFromChatCompletionResult(String chatCompletionResult) {
+
+        List<String> lines = Arrays.stream(chatCompletionResult.split("\n"))
+                .filter(message -> !message.isBlank())
+                .map(String::trim)
+                .toList();
+        Set<CollectionCategory> categories = new HashSet<>();
+        for (String line : lines) {
+            Matcher matcher = CHAT_COMPLETION_RESULT_PATTERN.matcher(line);
+            if (!matcher.matches()) {
+                return null;
+            }
+            categories.add(CollectionCategory.valueOf(matcher.group(PATTERN_CATEGORY_GROUP_NAME)));
+        }
+
+        if (!(MIN_LIST_SIZE <= categories.size() && categories.size() <= MAX_LIST_SIZE)) {
+            return null;
+        }
+        return categories.stream().sorted(Comparator.comparing(Enum::name)).toList();
+    }
 }
