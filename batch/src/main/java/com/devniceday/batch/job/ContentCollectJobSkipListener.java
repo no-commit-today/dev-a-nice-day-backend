a9950dd25e7eb9ee2167ec58b3859337b0db427e
Youngrae Cho <yrc9229@gmail.com>
2024-11-25 22:15:55 +0900

    feat(batch): 예외 로깅 이동

--- batch/src/main/java/com/devniceday/batch/job/ContentCollectJobSkipListener.java
+++ batch/src/main/java/com/devniceday/batch/job/ContentCollectJobSkipListener.java
@@ -3,12 +3,16 @@ package com.devniceday.batch.job;
 import com.devniceday.batch.domain.SubscriptionExceptionProcessor;
 import com.devniceday.storage.db.core.BatchCollectedContentEntity;
 import com.devniceday.storage.db.core.BatchSubscriptionEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.SkipListener;
 
 import java.util.List;
 
 public class ContentCollectJobSkipListener
         implements SkipListener<BatchSubscriptionEntity, List<BatchCollectedContentEntity>> {
+
+    private static final Logger log = LoggerFactory.getLogger(ContentCollectJobSkipListener.class);
 
     private final SubscriptionExceptionProcessor processor;
 
@@ -18,6 +22,7 @@ public class ContentCollectJobSkipListener
 
     @Override
     public void onSkipInProcess(BatchSubscriptionEntity item, Throwable t) {
+        log.error("컨텐츠 수집 중 오류 발생하여 disable 처리 합니다. subscriptionId={}", item.getId(), t);
         processor.process(item.getId(), (Exception) t);
     }
 }
