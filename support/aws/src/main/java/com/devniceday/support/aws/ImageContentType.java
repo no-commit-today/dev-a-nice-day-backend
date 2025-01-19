6904c88159b194ec8699595e0afc44a66d2f8414
Youngrae Cho <yrc9229@gmail.com>
2025-01-19 14:45:17 +0900

    feat(aws): aws 모듈 추가

---
+++ support/aws/src/main/java/com/devniceday/support/aws/ImageContentType.java
@@ -0,0 +1,34 @@
+package com.devniceday.support.aws;
+
+
+import jakarta.annotation.Nullable;
+
+import java.util.Map;
+
+public record ImageContentType(
+        @Nullable String value
+) {
+
+    private static final Map<String, String> TYPE_TO_EXT = Map.of(
+            "image/jpeg", "jpeg",
+            "image/png", "png",
+            "image/gif", "gif",
+            "image/bmp", "bmp",
+            "image/webp", "webp",
+            "image/svg+xml", "svg",
+            "image/tiff", "tif",
+            "image/x-icon", "ico",
+            "image/vnd.microsoft.icon", "ico"
+    );
+
+    public boolean supports() {
+        if (value == null) {
+            return false;
+        }
+        return TYPE_TO_EXT.containsKey(value.split(";")[0]);
+    }
+
+    public String ext() {
+        return TYPE_TO_EXT.get(value);
+    }
+}
---
+++ support/aws/src/main/java/com/devniceday/support/aws/NotSupportedImageException.java
@@ -0,0 +1,16 @@
+package com.devniceday.support.aws;
+
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+public class NotSupportedImageException extends RuntimeException {
+
+    NotSupportedImageException(Resource resource) {
+        super("resource=" + resource);
+    }
+
+    NotSupportedImageException(Resource resource, IOException e) {
+        super("resource=" + resource, e);
+    }
+}
