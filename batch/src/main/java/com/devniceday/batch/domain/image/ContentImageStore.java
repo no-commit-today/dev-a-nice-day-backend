06c57b7f980cb351be949a8f75d205e4f4d7e94c
Youngrae Cho <yrc9229@gmail.com>
2025-01-19 21:12:37 +0900

    feat(batch): aws 프로퍼티 세팅

--- batch/src/main/java/com/devniceday/batch/domain/image/ContentImageStore.java
+++ batch/src/main/java/com/devniceday/batch/domain/image/ContentImageStore.java
@@ -5,6 +5,7 @@ import com.devniceday.storage.db.core.BatchImageEntity;
 import com.devniceday.storage.db.core.BatchImageJpaRepository;
 import com.devniceday.support.aws.Image;
 import com.devniceday.support.aws.ImageUploader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -17,23 +18,26 @@ public class ContentImageStore {
     private final ImageUploader imageUploader;
     private final BatchImageJpaRepository batchImageJpaRepository;
     private final UuidHolder uuidHolder;
+    private final String bucketName;
 
     public ContentImageStore(
             ImageClient imageClient,
             ImageUploader imageUploader,
             BatchImageJpaRepository batchImageJpaRepository,
-            UuidHolder uuidHolder
+            UuidHolder uuidHolder,
+            @Value("${app.image.s3.bucket-name}") String bucketName
     ) {
         this.imageClient = imageClient;
         this.imageUploader = imageUploader;
         this.batchImageJpaRepository = batchImageJpaRepository;
         this.uuidHolder = uuidHolder;
+        this.bucketName = bucketName;
     }
 
     public long store(String originUrl, String dirToStore) {
         Image image = imageClient.get(originUrl);
         String storedName = createStoredName(image.contentType().ext(), dirToStore);
-        URL url = imageUploader.upload(image, "techswipe-images", storedName);
+        URL url = imageUploader.upload(image, bucketName, storedName);
 
         return batchImageJpaRepository.save(new BatchImageEntity(url.toString(), originUrl, storedName)).getId();
     }
